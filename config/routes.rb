afb48698724a736e7b2a2843f05dcef2ed92cd43
Rodrigo Haenggi <rodrigo@codegestalt.com>
2014-05-20 21:30:43 +0200

    fuck www

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,12 @@
 Rails.application.routes.draw do
+
+  # we don't want www
+  constraints(:host => /^www\./) do
+    get "(*x)" => redirect { |params, request|
+      URI.parse(request.url).tap {|url| url.host.sub!('www.', '') }.to_s
+    }
+  end
+
   root 'pages#home'
 
   get '/home', to: "pages#home"
